d3e800b62771bb0464306fc9c5fa4e96a5f85435
rahuljoshi02 <rahulsj@uci.edu>
2025-03-14 05:54:05 +0000

    error, adjacent, and generate ladder functions implemented

---
+++ src/ladder.cpp
@@ -0,0 +1,53 @@
+#include "ladder.h"
+
+
+
+void error(string word1, string word2, string msg) {
+    cerr << word1 << ", " << word2 << ": " << msg;
+}
+
+bool edit_distance_within(const std::string& str1, const std::string& str2, int d) {
+
+}
+
+
+bool is_adjacent(const string& word1, const string& word2) {
+    if (word1.length() != word2.length()) { return false; }
+    int counter = 0;
+    for (int i = 0; i < word1.length(); ++i) {
+        if (word1[i] != word2[i]) {
+            counter++;
+            if (counter > 1) { return false; }
+        }
+    }
+    return counter == 1;
+}
+
+
+vector<string> generate_word_ladder(const string& begin_word, const string& end_word, const set<string>& word_list) {
+    queue<vector<string>> ladder_queue;
+    ladder_queue.push(begin_word);
+
+    set<string> visited;
+    visited.insert(begin_word);
+
+    while (!ladder_queue.empty()) {
+        ladder = ladder_queue.front();
+        ladder_queue.pop();
+        string last_word = ladder.back();
+        for (const string & word : word_list) {
+            if (is_adjacent(end_word, word)) {
+                if (!visited.contains(word)) {
+                    visited.insert(word);
+                    vector<string> new_ladder = ladder;
+                    new_ladder.push_back(word);
+                    if (word == end_word) {
+                        return new_ladder;
+                    }
+                    ladder_queue.push(new_ladder);
+                }
+            }
+        }
+    }
+    return {};
+}
