e472ccbbd61641519eadaf3d5d1a2ff4a3b7d4c8
rahuljoshi02 <rahulsj@uci.edu>
2025-03-17 01:37:36 +0000

    extract shortest path implemented

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -27,7 +27,17 @@ vector<int> dijkstra_shortest_path(const Graph& G, int source, vector<int>& prev
     return distances;
 }
 
-
+vector<int> extract_shortest_path(const vector<int>& distances, const vector<int>& previous, int destination) {
+    vector<int> path;
+    if(distances[destination] == INF) {
+        return path;
+    }
+    for (int i = destination; i != -1; i = previous[i]) {
+        path.push_back(i);
+    }
+    reverse(path.begin(), path.end());
+    return path;
+}
 
 void print_path(const vector<int>& v, int total) {
     for (int i = 0; i < total; ++i) {
